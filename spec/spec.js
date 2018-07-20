f95333aeae05689f29051fd0bb78e4e34771959e
Volha Valasevich <volha_valasevich@epam.com>
2018-07-20 14:34:59 +0300

    in progress

---
+++ spec/spec.js
@@ -0,0 +1,73 @@
+const webdriver = require("selenium-webdriver");
+const by = webdriver.By;
+const Util = require("./support/util.js")
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 40000;
+
+describe('T-Mobile Website', () => {
+
+    let browser;
+
+    beforeAll(() => {
+        browser = new Util();
+    })
+
+    afterAll(() => {
+        browser.quit();
+    })
+
+    describe('/ Mobile plans page', () => {
+
+        beforeEach(() => {
+            return browser.getStartPage().then(() => {
+                browser.clickLink("PLANS");
+            }).then(() => {
+                browser.sleep(3000);
+            })
+        })
+
+        fit('should have correct title', () => {
+            browser.getTitle().then((title) => {
+                return expect(title).toEqual("Cell Phone Plans | Family Plans | Compare Cell Phone Plans | T-Mobile");
+            })
+        })
+
+        it('should change the price after moving the slider', () => {
+            return browser.dragSliderToTick("second").then(() => {
+                return browser.findElement(by.css("div.price-container.active div.price")).getText();
+            }).then((price) => {
+                return expect(price).toEqual("60");
+            })
+        })
+
+        describe('/ Military Plans Page', () => {
+
+            beforeEach(() => {
+                browser.findElement(by.css('a[data-analytics-id="WEB-26806-military-available -buttonCta"]')).click().then(() => {
+                    browser.sleep(3000);
+                })
+            })
+
+            it('should have correct title', () => {
+                browser.getTitle().then((title) => {
+                    return expect(title.length).toEqual("T-Mobile ONE Military Phone Plans | Discounts & More | T-Mobile");
+                })
+            })
+
+            xit('should have an option for verifying military status', () => {
+                browser.waitUntilPresent(by.linkText("Verify military status")).then()
+                browser.findElements(by.linkText("Verify military status")).then((found) => {
+                    return expect(found.length).toBeGreaterThan(0);
+                })
+            })
+
+            it('should change the price after moving the slider', () => {
+                return browser.dragSliderToTick("second").then(() => {
+                    return browser.findElement(by.css("div.price-container.active div.price")).getText();
+                }).then((price) => {
+                    return expect(price).toEqual("40");
+                })
+            })
+        })
+    })
+})
---
+++ spec/support/util.js
@@ -0,0 +1,68 @@
+const webdriver = require("selenium-webdriver");
+const by = webdriver.By;
+const until = webdriver.until;
+
+class Util {
+
+    constructor() {
+        this.browser = this.createDriver();
+    }
+
+    createDriver() {
+        const driver = new webdriver.Builder()
+            .usingServer('http://localhost:4444/wd/hub')
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .build();
+        driver.manage().timeouts().implicitlyWait(20000);
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    getStartPage() {
+        return this.browser.get("https://www.t-mobile.com/");
+    }
+
+    clickLink(text) {
+        return this.browser.findElement(by.linkText(text)).click();
+    }
+
+    dragSliderToTick(pos) {
+        return this.waitUntilPresent(by.css('input.slider-range')).then((slider) => {
+            switch (pos) {
+                case "second": case "third": {
+                    return this.browser.actions().dragAndDrop(slider, this.browser.findElement(by.css(`div.${pos}-tick`))).mouseUp().perform();
+                }
+                case "first": {
+                    return this.browser.actions().dragAndDrop(slider, this.browser.findElement(by.css(`div.item-alt-one`))).mouseUp().perform();
+                }
+                case "fourth": break;
+            }
+        })
+    }
+
+    getTitle() {
+        return this.browser.getTitle();
+    }
+
+    findElement(locator) {
+        return this.browser.findElement(locator);
+    }
+
+    findElements(locator) {
+        return this.browser.findElements(locator);
+    }
+
+    sleep(time) {
+        return this.browser.sleep(time);
+    }
+
+    waitUntilPresent(locator) {
+        return this.browser.wait(until.elementLocated(locator), 5000);
+    }
+
+    quit() {
+        this.browser.close();
+    }
+}
+
+module.exports = Util;
